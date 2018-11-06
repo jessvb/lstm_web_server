7a93bc67a0c88b0d29e0a3df2b2a64a699473437
tomiyee <tommy.and.heng@gmail.com>
2018-11-06 17:18:43 -0500

    Working Version with Multiple Loaded Models

--- index.js
+++ index.js
@@ -5,16 +5,129 @@ const url = require('url');
 const http = require('http');
 const port = 1234;
 
-// variables
+/* ============================================================= */
+/* ========================== Variables ======================== */
+/* ============================================================= */
+
+// Log Settings
+const LOG_QUERY_INPUTS  = true;
+const LOG_SEED          = true;
+// Logs to the console
+const LOG_TEXT_GEN_PROGRESS = true;
+
+// This is the default model given in case there is no model requested by the url
+const DEFAULT_MODEL = 'nietzsche';
+const DEFAULT_OUTPUT_LEN = 100;
+
+// Stores the location of the tfjs model for the model with the given name
+// the names are in the form <DATASET>_<Epochs trained>
+let modelFileNames = {
+  aliceInWonderland_1:  "AiW-1.json" ,
+  aliceInWonderland_5:  "AiW-5.json" ,
+  aliceInWonderland_20: "AiW-20.json",
+
+  drSeuss_1:  "dr-seuss-1.json" ,
+  drSeuss_5:  "dr-seuss-5.json" ,
+  drSeuss_10: "dr-seuss-10.json",
+  drSeuss_20: "dr-seuss-20.json",
+
+  narnia_1_1: "narnia-1-1.json",
+  narnia_1_5: "narnia-1-5.json",
+  narnia_1_10:"narnia-1-10.json",
+  narnia_1_20:"narnia-1-20.json",
+
+  wizardOfOz_1:  "WoOz-1.json" ,
+  wizardOfOz_5:  "WoOz-5.json" ,
+  wizardOfOz_20: "WoOz-20.json",
+
+  nietzsche: 'nietzsche.json',
+  harryPotter: 'harryPotter.json',
+};
+// The variable where all the model objects will be stored and used
+const models = {
+  aliceInWonderland_1:  null ,
+  aliceInWonderland_5:  null ,
+  aliceInWonderland_20: null,
+
+  drSeuss_1:  null ,
+  drSeuss_5:  null ,
+  drSeuss_10: null,
+  drSeuss_20: null,
+
+  narnia_1_1: null,
+  narnia_1_5: null,
+  narnia_1_10:null,
+  narnia_1_20:null,
+
+  wizardOfOz_1:  null ,
+  wizardOfOz_5:  null ,
+  wizardOfOz_20: null,
+
+  nietzsche: 'nietzsche.json',
+  harryPotter: 'harryPotter.json'
+}
+// Charsets shared between models
+// (e.g. for models trained on same dataset saved at different epochs)
+const alice = [
+  "\n", " ", "!", "(", ")", "*", ",", "-", ".", ":", ";", "?", "[", "]",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "—", "‘",
+  "’", "“", "”"
+];
+const narnia_1 = [
+  "\n", " ", "!", "\"", "'", "(", ")", ",", "-", ".", ":", ";", "?", "_",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+const drSeuss = [
+  "\n", " ", "!", "\"", "$", "'", "(", ")", ",", "-", ".", "/", "0", "1",
+  "3", "4", "6", "8", "9", ":", ";", "?", "a", "b", "c", "d", "e", "f",
+  "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+  "u", "v", "w", "x", "y", "z", "‘"
+];
+const woOz = [
+  "\n", " ", "!", "\"", "'", "(",")", ",", "-", ".", "0", "1", "2", "3",
+  "4", "5", "6", "7", "8", "9", ":", ";", "?", "a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+  "t", "u", "v", "w", "x", "y", "z"
+];
+const nancyDrew = [
+  "\n", " ", "!", "\"", "'", ",", "-", ".", "0", "1", "4", "5", "8", ":",
+  ";", "?", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+  "£", "—", "’"
+];
+// Mapping of individual models with their charsets
 const charSets = {
-  drSeuss: [
-    'T', 'h', 'e', ' ', 'C', 'a', 't', 'i', 'n', 'H', '↵', 'B', 'y',
-    'D', 'r', '.', 'S', 'u', 's', 'd', 'o', 'I', 'w', 'p', 'l', 'A',
-    'c', ',', 'W', '"', 'm', 'g', '!', 'b', 'k', 'N', 'U', 'M', 'P',
-    'j', '?', 'v', 'L', 'f', 'Y', 'O', 'F', '-', 'x', 'X', '\'', 'E',
-    'G', 'K', 'q', 'J', 'R', 'V', 'z', 'Q', '<94>', ':', 'â', '<80>', '<99>',
-    ';', 'Z', '(', '9', '8', '3', '/', '4', ')', '“', '’', '…', '”',
-    '‘', '—', '1', '0', '$', ' ', '­', ';', '6'
+  aliceInWonderland_1: alice,
+  aliceInWonderland_5: alice,
+  aliceInWonderland_20: alice,
+
+  drSeuss_1: drSeuss,
+  drSeuss_5: drSeuss,
+  drSeuss_10: drSeuss,
+  drSeuss_20: drSeuss,
+
+  narnia_1_1: narnia_1,
+  narnia_1_5: narnia_1,
+  narnia_1_10:narnia_1,
+  narnia_1_20:narnia_1,
+
+  nancyDrew_1: nancyDrew,
+  nancyDrew_5: nancyDrew,
+  nancyDrew_20: nancyDrew,
+  nancyDrew_40: nancyDrew,
+
+  wizardOfOz_1: woOz,
+  wizardOfOz_5: woOz,
+  wizardOfOz_20: woOz,
+
+  harryPotter: [
+    "\n", " ", "!", "\"", "'", "(", ")", "*", ",", "-", ".",
+    "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "=",
+    "?", "\\", "^", "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+    "y", "z", "}", "~", "�", "­", "é", "ü", "–", "‘", "’", "“"
   ],
   nietzsche: [
     'P', 'R', 'E', 'F', 'A', 'C', '↵', 'S', 'U', 'O', 'I', 'N', 'G', ' ',
@@ -24,45 +137,52 @@ const charSets = {
     'Y', 'K', 'q', 'M', 'x', 'J', '1', '8', '5', '2', '3', '_', '4', '6',
     '7', '9', '0', 'Q', 'X', '[', ']', 'Z', 'ä', '=', 'æ', 'ë', 'é', 'Æ'
   ],
-  harryPotter: [
-    "\n", " ", "!", "\"", "'", "(", ")", "*", ",", "-", ".",
-    "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "=",
-    "?", "\\", "^", "_", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-    "y", "z", "}", "~", "�", "­", "é", "ü", "–", "‘", "’", "“"
-  ],
-
-
 };
-
-//let selectedText = 'drSeuss';
-// let selectedText = 'nietzsche';
-let selectedText = 'harryPotter';
-
-// VARIABLES IN LOADABLE CLASS CONSTRUCTOR
-const charSet = charSets[selectedText];
-const charSetSize = charSet.length;
-
-// continue regular variables
-let modelFileNames = {
-  drSeuss: 'drSeuss.json',
-  nietzsche: 'nietzsche.json',
-  harryPotter: 'harryPotter.json',
-};
-
-let generateLengthInput = 100;
+// Smaller Temperature means less creative and more sensible
 let temperatureInput = 0.75;
 // let seedTextInput = 'This is a seed input. Hopefully it works.'; TODO THIS IS LENGTH 40 VS 60 FOR NIET VS HARRYPOTTER
 let seedTextInput = "This is a seed input. Hopefully it works and we'll get results.";
-let generatedTextInput = '';
-
 // const sampleLen = 40; TODO CHANGE THIS TO 40/60 FOR NIET VS HARRYPOTTER
-const sampleLen = 60;
+const sampleLen = 40;
 
 
-//////////////////////////////////////
-// functions from original utils.js //
-//////////////////////////////////////
+
+/**
+ * Loads the text generator model at the given file path and returns it
+ */
+async function loadModel (selectedModel) {
+  // acquires model
+  const path = 'file://./models/' + selectedModel
+  const model = await tf.loadModel(path);
+  // returns the model and its accompanying data
+  return {
+    "model": model,
+    "lstmLayersSizesInput": lstmLayerSizes(model)
+  };
+}
+
+
+
+/**
+ * Loads every model in the modelFileNames into the global variable "models"
+ */
+async function setupModels () {
+  console.log("\nBeginning to load all registered files.");
+  for (let selectedModel in modelFileNames) {
+    // loadModel returns {model:model,lstmLayersSizesInput:lstmLayersSizes}
+    let model = await loadModel (modelFileNames[selectedModel]);
+    models[selectedModel] = model;
+    console.log("    Loaded Model: " + selectedModel)
+  }
+  console.log("Done loading all registered files.\n")
+}
+
+
+/* ============================================================= */
+/* ============= functions from original utils.js ============= */
+/* ============================================================= */
+
+
 /**
  * Draw a sample based onprobabilities.
  *
@@ -92,14 +212,10 @@ function sample(preds, temperature) {
     return tf.multinomial(unnormalized, 1, null, false).dataSync()[0];
   });
 }
-//////////////////////////////////////
-// functions from original utils.js //
-//////////////////////////////////////
 
-
-/////////////////////////////////////////////////////////////
-// functions from original LoadableLSTMTextGenerator class //
-/////////////////////////////////////////////////////////////
+/* ============================================================= */
+/* ==== functions from orig LoadableLSTMTextGenerator class ==== */
+/* ============================================================= */
 
 /**
   * Get a representation of the sizes of the LSTM layers in the model.
@@ -120,19 +236,24 @@ function lstmLayerSizes(model) {
   return layerSizes.length === 1 ? layerSizes[0] : layerSizes;
 }
 
+
+
 /**
-   * Generate text using the LSTM model.
-   *
-   * @param {number[]} sentenceIndices Seed sentence, represented as the
-   *   indices of the constituent characters.
-   * @param {number} length Length of the text to generate, in number of
-   *   characters.
-   * @param {number} temperature Temperature parameter. Must be a number > 0.
-   * @returns {string} The generated text.
-   */
-async function genText(sentenceIndices, length, temperature, model, charSet, charSetSize, sampleLen) {
+ * Generate text using the LSTM model.
+ *
+ * @param {number[]} sentenceIndices Seed sentence, represented as the
+ *   indices of the constituent characters.
+ * @param {number} length Length of the text to generate, in number of
+ *   characters.
+ * @param {number} temperature Temperature parameter. Must be a number > 0.
+ * @returns {string} The generated text.
+ */
+async function genText(currentModel, sentenceIndices, length, temperature, model, charSet, charSetSize, sampleLen) {
+
   generatedTextInput = '';
+
   console.log('Generating text...');
+  console.log("    Length: " + length);
 
   const temperatureScalar = tf.scalar(temperature);
 
@@ -153,7 +274,7 @@ async function genText(sentenceIndices, length, temperature, model, charSet, cha
     // Sample randomly based on the probability values.
     const winnerIndex = sample(tf.squeeze(output), temperatureScalar);
     const winnerChar = charSet[winnerIndex];
-    await onTextGenerationChar(winnerChar);
+    await onTextGenerationChar(winnerChar, length);
 
     generated += winnerChar;
     sentenceIndices = sentenceIndices.slice(1);
@@ -167,35 +288,35 @@ async function genText(sentenceIndices, length, temperature, model, charSet, cha
 }
 
 
-////////////////////////////////////////////////////////
-// end functions from LoadableLSTMTextGenerator class //
-////////////////////////////////////////////////////////
-
 
 /**
  * Load generator (lstm model) function
  */
-async function loadGen() {
-  const path = 'file://./models/' + modelFileNames[selectedText]
-  const model = await tf.loadModel(path);
+async function loadGen(modelName) {
 
+  const path = 'file://./models/' + modelFileNames[modelName]
+  const model = await tf.loadModel(path);
 
   lstmLayersSizesInput = lstmLayerSizes(model);
   model.summary();
   return model;
 };
 
+
+
 /**
  * A function to call each time a character is obtained during text generation.
  *
  * @param {string} char The just-generated character.
  */
-async function onTextGenerationChar(charac) {
+async function onTextGenerationChar(charac, len) {
   generatedTextInput += charac;
   const charCount = generatedTextInput.length;
-  const generateLength = generateLengthInput;
+  const generateLength = len;
   const currStatus = `Generating text: ${charCount}/${generateLength} complete...`;
-  //console.log(currStatus);
+
+  if(LOG_TEXT_GEN_PROGRESS && charCount % Math.round(generateLength/5) == 0)
+    console.log(currStatus);
   //console.log('character in onTextGenerationChar: '+charac);
   if (charCount / generateLength == 1) {
     console.log(generatedTextInput);
@@ -203,24 +324,41 @@ async function onTextGenerationChar(charac) {
   await tf.nextFrame();
 }
 
+
+
 /**
  * Use `textGenerator` to generate random text, show the characters in the
  * console as they are generated one by one.
+ * @param currentModel, the string name of the current model
+ *
 */
-async function generateText(model, charSet, charSetSize, sampleLen, seedTextInput) {
+async function generateText(currentModel, sampleLen, outputLen, seedTextInput) {
   try {
+    // loads the currentModel's preloaded model
+    let model = models[currentModel].model
+    // Loads the charset for the currentModel
+    let charSet = charSets[currentModel];
+    // Determines the length of the charset
+    let charSetSize = charSet.length;
+
+    // ERROR 1 - No Model
     if (model == null) {
       console.log('ERROR: Please load text data set first.');
       return;
     }
-    const generateLength = generateLengthInput;
+
+    const generateLength = outputLen;
     const temperature = temperatureInput;
+
+    // ERROR 2 - Improper value for generateLength
     if (!(generateLength > 0)) {
       console.log(
         `ERROR: Invalid generation length: ${generateLength}. ` +
         `Generation length must be a positive number.`);
       return;
     }
+
+    // ERROR 3 - Temperature must be positive and between (0,1]
     if (!(temperature > 0 && temperature <= 1)) {
       console.log(
         `ERROR: Invalid temperature: ${temperature}. ` +
@@ -230,6 +368,8 @@ async function generateText(model, charSet, charSetSize, sampleLen, seedTextInpu
 
     let seedSentence;
     let seedSentenceIndices;
+
+    // ERROR 4 - Invalid Seed Text Length
     if (seedTextInput.length === 0) {
       console.log(
         `ERROR: seed sentence length is zero. Seed Sentence: ` +
@@ -250,12 +390,14 @@ async function generateText(model, charSet, charSetSize, sampleLen, seedTextInpu
       seedSentenceIndices = [];
       for (let i = 0; i < seedSentence.length; ++i) {
         seedSentenceIndices.push(
-          charSets[selectedText].indexOf(seedSentence[i]));
+          charSets[currentModel].indexOf(seedSentence[i]));
       }
     }
 
+    console.log("generateLength before calling genText: " + generateLength)
+
     const sentence = await genText(
-      seedSentenceIndices, generateLength, temperature, model, charSet, charSetSize, sampleLen);
+      currentModel, seedSentenceIndices, generateLength, temperature, model, charSet, charSetSize, sampleLen);
     generatedTextInput = sentence;
     const currStatus = 'Done generating text.';
     console.log(currStatus);
@@ -268,45 +410,98 @@ async function generateText(model, charSet, charSetSize, sampleLen, seedTextInpu
 }
 
 
+
+
 /**
  * Function to set up the model / start the server / start listening for requests
  */
 async function setUp() {
-  // load the lstm model
-  let model = await loadGen();
+  // loads all the models
+  setupModels();
 
   // set up the server
   const app = http.createServer(async function (request, response) {
+
+    // TO-DO
+    // Respond to the favicon.ico request properly if necessary
+    if(request.url == "/favicon.ico")
+      return response.end();
+
+    console.log("\n\nAnswering New Request!!!\n")
     var q, respJSON;
 
     // respond to query with generated text
     q = url.parse(request.url, true).query;
-    if (q.inputText) {
-      console.log('there is input text!');
-      let generatedText = '';
-      if (q.inputText == 'test') {
-        console.log('this is a test');
-        generatedText = 'test returned correctly';
-      } else {
-        if (q.inputText.length < sampleLen) {
-          console.log('inputText is too short. using previous seed.');
-        } else {
-          seedTextInput = q.inputText;
-        }
 
-        console.log('seed: ' + seedTextInput);
-        // Generate text and output in console.
-        try {
-          generatedText = await generateText(model, charSet, charSetSize, sampleLen, seedTextInput);
-        } catch (err) {
-          console.log(err);
-        }
-      }
-      respJSON = { generated: generatedText };
-    } else {
-      console.log('no input text.');
-      respJSON = { generated: null };
+    // Logs to the console the Data given in the url
+    if(LOG_QUERY_INPUTS){
+      console.log("Data From URL:")
+      console.log(q);
+      console.log("\n");
     }
+
+    // Reassigns the global variable which defaults to the DEFAULT_OUTPUT_LEN
+    outputLen = q.outputLength || DEFAULT_OUTPUT_LEN;
+
+    // Assigns a default model
+    let currentModel = DEFAULT_MODEL;
+
+    // If there is a query for the model
+    if(q.model) {
+      if(models[q.model])
+        currentModel = q.model;
+      else
+        console.log(`  Requested model (${q.model}) does not exist. Using default model.`);
+    }
+
+    // if there is no input text, return an error
+    if (!(q.inputText)) {
+      console.log('There was no input text provided. Throwing error...');
+      respJSON = { generated: null };
+      response.writeHead(200, { 'Content-Type': 'application/json', 'json': 'true' });
+      response.write(JSON.stringify(respJSON));
+      response.end();
+      return;
+    }
+
+    // This will store the model's generated text
+    let generatedText = '';
+
+    // Handles Tests
+    if (q.inputText == 'test') {
+      console.log('This is a test!');
+      generatedText = 'test returned correctly';
+      respJSON = { generated: generatedText };
+      response.writeHead(200, { 'Content-Type': 'application/json', 'json': 'true' });
+      response.write(JSON.stringify(respJSON));
+      response.end();
+      return;
+    }
+
+    // Determines if the input is of sufficient length
+    if (q.inputText.length < sampleLen) {
+      console.log('inputText is too short. Using previous seed.');
+    } else {
+      seedTextInput = q.inputText;
+    }
+
+    if(LOG_SEED) {
+      console.log('  Seed: ' + seedTextInput);
+    }
+
+    // Generate text and output in console.
+    try {
+      generatedText = await generateText(
+        currentModel,
+        sampleLen,
+        outputLen,
+        seedTextInput);
+    } catch (err) {
+      console.log(err);
+    }
+
+    // The responseJSON is made and returned.
+    respJSON = { generated: generatedText };
 
     response.writeHead(200, { 'Content-Type': 'application/json', 'json': 'true' });
     response.write(JSON.stringify(respJSON));
